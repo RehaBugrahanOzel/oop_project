7e05bff2fba163239706844d6b3a5801dad96815
Ezgi Korkmaz <korkmaz_ezgi@outlook.com>
2021-05-30 13:55:44 +0300

    Classes

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,19 @@
 
 public class Customer {
 
+	String nameSurname;
+	int phoneNumber;
+	String emailAddress;
+	int age;
+	boolean isStudent;
+	int memberNo;
+	
+	public Customer editCustomer (Customer cst) {
+		
+	}
+	public void removeCustomer (Customer cst) {
+		
+	}
+	
+	
 }
