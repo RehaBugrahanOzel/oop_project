2d7289f6b26ad50a49adf7a0eae66f68b969f41d
Berkbarut <bbarut287@gmail.com>
2021-06-17 15:18:55 +0300

    15.18

--- src/Management.java
+++ src/Management.java
@@ -1,17 +1,67 @@
 import java.util.*;
 public class Management {
 
-	Stack customer_stack = new Stack();
-	Stack user_stack = new Stack();
-	Stack play_stack = new Stack();
-	Stack seat_stack = new Stack();
+	static Stack customer_stack = new Stack();
+	static Stack user_stack = new Stack();
+	static Stack play_stack = new Stack();
+	static Stack seat_stack = new Stack();
 	
-	public void user_creation(String user_name, String password) {
+	public static void user_creation(String user_name, String password) {
 		User user = new User(user_name,password);
 		user_stack.push(user);
 	}
-	
-	public boolean user_check(Stack user_stack, String ) {
+	public static void test() {
+		Scanner myObj = new Scanner(System.in);
+	    System.out.println("1-Sign up");
+	    System.out.println("2-Log in");
+	    System.out.println("3-Remove user");
+	    int choice = myObj.nextInt();
+	    if(choice==1) {
+	    	System.out.println("Enter user name: ");
+	    	String username = myObj.nextLine();
+	    	System.out.println("Enter password: ");
+	    	String password = myObj.nextLine();
+	    	user_creation(username, password);
+	    	System.out.println("User created");
+	    	
+	    }
+	    if(choice==2) {
+	    	System.out.println("Enter user name: ");
+	    	String username = myObj.nextLine();
+	    	System.out.println("Enter password: ");
+	    	String password = myObj.nextLine();
+	    	User loginuser=new User(username,password);
+	    	boolean isUser=false;
+	    	
+	    	while(isUser==false) {
+	    		isUser=loginuser.loggedIn(user_stack, loginuser);
+	    		if(isUser==true) {
+		    		System.out.println("You are successfully logged in");
+		    		////BURAYA FONKSÝYON ÇAÐRILCAK ANA ÝÞLEMLER ÝÇÝN
+		    	}
+		    	else if(isUser==false) {
+		    		System.out.println("Wrong password or username");
+		    	}
+	    		
+	    	}
+	    }
+	    
+	    if(choice==3) {
+	    	System.out.println("Enter user name: ");
+	    	String username = myObj.nextLine();
+	    	System.out.println("Enter password: ");
+	    	String password = myObj.nextLine();
+	    	User loginuser=new User(username,password);
+	    	loginuser.removeUser(loginuser,user_stack);
+	    	System.out.println("User has been removed");
+	    }
+	    
+	    
+		
+		
+		
+		
 		
 	}
+	
 }
--- src/User.java
+++ src/User.java
@@ -10,19 +10,53 @@ public class User {
 		this.password = password;
 	}
 
-	public boolean loggedIn(Stack user_stack, String name, String password) {
+	public boolean loggedIn(Stack user_stack, User user) {
 		int size = user_stack.size();
 		Stack temp = new Stack();
+		boolean flag=false;
 		for (int i = 0; i < size; i++) {
-			temp.push(user_stack.pop());
-			
-			if (temp.peek()) {
-				
+			User tempuser = (User) user_stack.pop();
+			if(tempuser.userName.equals(user.userName)){
+				if(tempuser.password.equals(user.password)) {
+					flag = true;
+				}
+				else {
+					System.out.println("Wrong password");
+				}
 			}
+			temp.push(tempuser);
 		}
+		for (int i = 0; i < size; i++) {
+			user_stack.push(temp.pop());
+		}
+		
+		if(flag==false) {
+			System.out.println("User doesn't exist or password is wrong");
+		}
+		/*else if(flag==true) {
+			System.out.println("ANAN YANÝ GÝRÝÞ");
+		}*/
+		return flag;
 	}
 	
-	public User removeUser (User user) {
-		return user = null;
+	public void removeUser (User user,Stack user_stack) {
+		Stack temp = new Stack();
+		int size = user_stack.size();
+		for (int i = 0; i < size; i++) {
+			boolean flag=true;
+			User tempuser = (User) user_stack.pop();
+			if(tempuser.userName.equals(user.userName)){
+				if(tempuser.password.equals(user.password)) {
+					flag=false;
+				}
+			}
+			if(flag) {
+				temp.push(tempuser);
+			}
+		}
+		int tempsize = temp.size();
+		for (int i = 0; i < tempsize; i++) {
+			user_stack.push(temp.pop());
+		}
 	}
 }
