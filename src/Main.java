0f24cb6ff3ae5e824d911494909a3ae95f2207c2
Ezgi Korkmaz <korkmaz_ezgi@outlook.com>
2021-06-18 15:05:00 +0300

    15.04

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,4 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 public class Main {
 
 	public static void main(String[] args) {
